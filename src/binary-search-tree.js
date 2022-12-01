f3329152494442bf38e50ea3bc6f2144eca54d86
Nastya <nastyremez8439@gmail.com>
2022-12-01 18:52:02 +0300

    feat: test commit

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,8 +8,7 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 module.exports = class BinarySearchTree {
   root() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    
   }
 
   add(/* data */) {
