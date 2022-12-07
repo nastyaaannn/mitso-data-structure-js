3abc7d4d25efd77c166601800add7023a876dd4c
Nastya <nastyremez8439@gmail.com>
2022-12-08 00:01:08 +0300

    feat:stack.js done

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require("../extensions/index.js");
+//const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,18 +13,30 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  constructor()
+  {
+     this.stack = {}; 
+     this.count = 0;
   }
+  push(element) {
+    let size = this.count;
+    this.stack[size] = element;
+    this.count = size + 1;
+  } 
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    let size = this.count - 1;
+    let deletedData;
+
+    deletedData = this.stack[size];
+ 
+    delete this.stack[size];
+    this.count--;
+ 
+    return deletedData;
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.stack[this.count - 1];
   }
 };
